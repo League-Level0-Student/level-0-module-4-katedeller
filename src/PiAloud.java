fbf3514f9c66637b9e577350917a0eb0e5d10cce
league <league@wts7.attlocal.net>
2018-04-01 17:02:21 -0700

    Pi, Calculator

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -8,19 +8,32 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String pi = "3.14159265359";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+System.out.println(pi.charAt(0));
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+for(int i= 0; i<pi.length(); i++) { 
+	System.out.println(pi.charAt(i));
+	speak(pi.charAt(i));
+	char k = getInputFromUser();
+	if(k== pi.charAt(i+1)) {
+		System.out.println("Correct");
+	}
+}
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
+char k = getInputFromUser();
 	// *7. Compare the users' char to the next digit of Pi
+
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
+}
 
 	static void speak(char characterToSpeak) {
 		try {
@@ -45,5 +58,6 @@ public class PiAloud {
 	}
 
 }
+
 
 
--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -6,20 +6,38 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+String a =JOptionPane.showInputDialog("First Number equals");
+int a1 = Integer.parseInt(a);
+String b =JOptionPane.showInputDialog("Second Number equals");
+int b1 = Integer.parseInt(b);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
 		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "add", "subtract", "multiply", "divide" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+if(operation==0) JOptionPane.showMessageDialog(null, a1 + "+" + b1 + "=" + add( a1, b1));
+if(operation== 1) JOptionPane.showMessageDialog(null, a1 + "-" + b1 + "=" +subtract(a1, b1));
+if(operation== 2) JOptionPane.showMessageDialog(null, a1 + "x" + b1 + "=" +multiply(a1, b1));
+if(operation== 3) JOptionPane.showMessageDialog(null,a1 + "/" + b1 + "=" +div(a1, b1));
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
 
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
-
+static int add(int num1, int num2) {
+	return num1+num2;
+}
+static int subtract(int num1, int num2) {
+	return num1-num2;
+}
+static int multiply(int num1, int num2) {
+	return num1*num2; 
+}
+static int div(int num1, int num2) {
+	return num1/num2;
+}
 	// 4. Create similar methods for subtraction, multiplication and division.
+
 }
